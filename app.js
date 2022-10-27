b15823675c47464d76f7c589fadd2f47a8870cf3
Paulfresnel <paul.fresnel1@gmail.com>
2022-10-27 22:03:55 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const app = express();
+const port =3000;
+
+app.use(express.static('public'));
+
+app.listen(port,()=>{
+    console.log('app reunning on port 3000')
+})
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(__dirname + '/views/about.html');
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("Err404: Sorry can't find that!")
+  })
+
